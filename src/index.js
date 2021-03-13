f93c36322d2dbb6243df8db9409155d389daab21
github-prozhenik <djfiers@gmail.com>
2021-03-13 14:11:23 +0300

    feat:typical arrays problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+    if (typeof array === 'undefined' || !array.length){
   return 0;
+}
+return Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
+    if (typeof array === 'undefined' || !array.length){
   return 0;
+    }
+return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
+    if (typeof array === 'undefined' || !array.length){
   return 0;
+}
+let total = 0;
+for(let i = 0; i < array.length; i++) {
+    total += array[i]
+}
+let avg = total / array.length;
+return avg;
 }
