7af5b57e63ceec10eb8159a7047397ba6766ae47
github-prozhenik <djfiers@gmail.com>
2021-08-23 13:36:33 +0300

    feat: implement 3 functions: max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 
 exports.min = function min (array) {
-    if (typeof array === 'undefined' || !array.length){
-  return 0;
-}
-return Math.min.apply(null, array)
+    return array === undefined || array.length === 0 ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-    if (typeof array === 'undefined' || !array.length){
-  return 0;
-    }
-return Math.max.apply(null, array)
-}
+    return array === undefined || array.length === 0 ? 0 : Math.max(...array);
+}  
 
 exports.avg = function avg (array) {
-    if (typeof array === 'undefined' || !array.length){
-  return 0;
-}
-let total = 0;
-for(let i = 0; i < array.length; i++) {
-    total += array[i]
-}
-let avg = total / array.length;
-return avg;
+    return array === undefined || array.length === 0 ? 0 : array.reduce((a, b) => a + b) / array.length;
 }
